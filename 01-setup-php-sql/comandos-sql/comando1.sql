0837080d89e30d06855486d3cc3e836691b38c23
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-14 16:58:50 -0500

    Update SQL commands in comando1.sql for consistency and clarity

--- 01-setup-php-sql/comandos-sql/comando1.sql
+++ 01-setup-php-sql/comandos-sql/comando1.sql
@@ -20,5 +20,30 @@ INSERT hace para insertar filas en  nuestra tabla y agregarle valores
 INSERT INTO Users (nombre, email) VALUES ('Gabriel', 'gtb@mail.com');
 INSERT INTO Users (nombre, email) VALUES ('PEPE', 'gtb@mail.com');
 INSERT INTO Users (nombre, email) VALUES ('Juan', 'gtb@mail.com');
-INSERT INTO Users (nombre, email) VALUES ('LaDele', 'gtb@mail.com');
+INSERT INTO Users (nombre, email) VALUES ('LaDele', 'gt23b@mail.com');
 INSERT INTO Users (nombre, email) VALUES ('Jeolar', 'gtb@mail.com');
+
+/* DELETE sentencia, para borrar filas en una tabla basando en el criterio de seleccion*/
+
+DELETE FROM Users WHERE email='gtb@mail.com'
+
+/*UPDATE esta sentencia no permite actualizar campos con una clausula WHERE*/
+
+UPDATE Users SET nombre='Charles' WHERE email='gt23b@mail.com'
+
+/*recuperar record con SELECT*/
+
+SELECT * FROM Users 
+SELECT nombre FROM Users WHERE email='gt23b@mail.com'
+
+/*ordenar usando ORDER BY*/
+
+SELECT * FROM Users ORDER BY email
+
+/*LIKE se usa para obtener de ciertas columnas tengan algun caracter en especial o dato*/
+SELECT * FROM Users WHERE nombre LIKE '%e%'
+
+/* para contar la cantidad de filas que tiene una columna */
+
+SELECT COUNT(*) FROM Users;
+SELECT COUNT(*) FROM Users WHERE email='gt23b@mail.com' /*COUNT(*) 1 */
