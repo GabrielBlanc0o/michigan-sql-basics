e537987d8f22572ecf70656520e850481ef4e34d
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-14 16:42:16 -0500

    Enhance SQL setup script with additional comments and structure for clarity

--- 01-setup-php-sql/comandos-sql/comando1.sql
+++ 01-setup-php-sql/comandos-sql/comando1.sql
@@ -1,9 +1,24 @@
 
+/* Crear bd con tipo utf8*/
 CREATE DATABASE People DEFAULT CHARACTER SET utf8;
-
+/* Usamos la base de datos*/
 USE People;
 
+/*creamos tablas*/
 CREATE TABLE Users (
     nombre VARCHAR(128),
    email VARCHAR(128)
 );
+/*mostramos el contenido de la tabla */
+describe Users;
+
+/*
+
+INSERT hace para insertar filas en  nuestra tabla y agregarle valores 
+*/
+
+INSERT INTO Users (nombre, email) VALUES ('Gabriel', 'gtb@mail.com');
+INSERT INTO Users (nombre, email) VALUES ('PEPE', 'gtb@mail.com');
+INSERT INTO Users (nombre, email) VALUES ('Juan', 'gtb@mail.com');
+INSERT INTO Users (nombre, email) VALUES ('LaDele', 'gtb@mail.com');
+INSERT INTO Users (nombre, email) VALUES ('Jeolar', 'gtb@mail.com');
